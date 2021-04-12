c2e4cfa95ac69208b2f7c4a7fbdbea0d66dbcce6
Frank Pena <frankrosario1998@gmail.com>
2021-04-11 21:55:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+
+export default function ExpoRadioButton(props) {
+    const { selected, value,size, onSelected, children, containerStyle,radioStyle,radioBackground } = props;
+
+
+    function isSelected() {
+
+        return selected == value;
+    }
+
+    function getSize(){
+        if(size) return size;
+
+        return 25;
+    }
+
+    function getBackground(){
+        if(radioBackground) return radioBackground;
+        return "blue";
+    }
+
+    return (
+        <TouchableOpacity onPress={() => onSelected(value)}
+            style={{ flexDirection: 'row', alignItems: 'center',...containerStyle }}
+        >
+            <View style={{
+                borderWidth: 2, borderColor: '#CCC',
+                padding: 2,
+                width: getSize(), height: getSize(), borderRadius: 50,
+                ...radioStyle
+
+            }}>
+                <View
+                    style={{
+                        backgroundColor: isSelected() ? getBackground() : 'rgba(0,0,0,0)',
+                        width: '100%',
+                        height: '100%',
+                        borderRadius: 50,
+                    }}
+                ></View>
+            </View>
+
+            <View style={{ marginLeft: 3 }}>
+                {children}
+            </View>
+
+        </TouchableOpacity>
+    )
+}
